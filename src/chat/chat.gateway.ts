874edd4775bf7383fb47bcf5d91fbb3c66ff984a
mehran feriduni <m3hr4nus@gmail.com>
2024-11-13 16:40:48 +0330

    connected user and disconnected init

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,14 +1,20 @@
-import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
 
 @WebSocketGateway({cors:{origin:"*"}})
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection,OnGatewayDisconnect{
+ @WebSocketServer() server:Server
   afterInit(server: any) {
     console.log("connction init")
   }
   handleConnection(client: any, ...args: any[]) {
-    
+    const {sockets}= this.server.sockets
+    console.log(`Client Id : ${client.id} Connected`)
+    console.log(`Online User: ${sockets.size}`)
   }
   handleDisconnect(client: any) {
-    
+    const {sockets}= this.server.sockets
+    console.log(`Client Id : ${client.id} Disconnected`)
+    console.log(`Online User: ${sockets.size}`)
   }
 }
